d7329268eab039b281178524e2607fc9ce3f2e4b
Dagnm <dagnachew16@gmail.com>
2025-07-28 13:28:59 +0300

    Tic-Tac Game

--- script.js
+++
@@ -1,6 +0,0 @@
-function toogleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".humburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
